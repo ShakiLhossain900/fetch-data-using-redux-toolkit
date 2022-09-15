12ae42a90c87b1b2a156e22e8f32348b535c1ec3
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-15 16:37:58 +0600

    adding all the logic but error is here

--- src/features/posts/PostView.js
+++ src/features/posts/PostView.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {fetchPosts} from "./PostSlice";
 
 const PostView = () => {
-    return (
-        <div>
-            <h1>Post view</h1>
-        </div>
-    );
+  const { isLoading, posts, error } = useSelector((state) => state.posts);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
+  return (
+    <div>
+      <h1>fetch data using react toolkit</h1>
+      {isLoading && <h3>Loading...</h3>}
+      {error && <h3>{error}</h3>}
+      <section>
+        {posts &&
+          posts.map((post) => {
+            return (
+              <article>
+                <h5>{post.title}</h5>
+                <p>{post.body}</p>
+              </article>
+            );
+          })}
+      </section>
+    </div>
+  );
 };
 
 export default PostView;
